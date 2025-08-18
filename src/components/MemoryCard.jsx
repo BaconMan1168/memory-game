df6d6591b1df6fb9cf3f479bce01e86993e14d26
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-18 16:17:20 +0800

    Added state to MemoryCard component to keep track of if the card has been clicked or not

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,10 +1,21 @@
+import {useState} from "react";
+
 export default function MemoryCard({gifId}){
+    const [isClicked, setIsClicked] = useState(false);
+
+    function handleClick(){
+        if (!isClicked){
+            setIsClicked(true)
+        }
+    }
+
+
     const gif = fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=3jCZ9tx5mumrMUeoRZXCygZzChJIb82z`).then(
         res => res.json()
     ).then(data => { return {imgSrc: data.data.images.original.url, imgName: data.data.title}})
 
     return (
-        <div className="card">
+        <div className="card" onClick={handleClick}>
             <img src={gif.imgSrc} alt=""></img>
             <h2>{gif.imgName}</h2>
         </div>
