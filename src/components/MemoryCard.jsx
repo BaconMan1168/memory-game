49efb47419db9bb192ebf67a4d7e6c72763c95ed
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-18 13:10:55 +0800

    MemoryCard component now takes in a gifId prop and returns jsx for an image and label

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export default function MemoryCard({gifId}){
+    const gif = fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=3jCZ9tx5mumrMUeoRZXCygZzChJIb82z`).then(
+        res => res.json()
+    ).then(data => data.data.images.original.url)
+
+    return (
+        <div className="card">
+            <img src={gif} alt={"Invincible flying"}></img>
+            <h2>{"Invincible"}</h2>
+        </div>
+    )
+}
