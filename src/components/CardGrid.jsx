37a59cc6bbd4db706483f185e0a5bb64ec97d643
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-18 13:29:13 +0800

    Giphy API call now returns an object containg the gif src and name so I can use both when returning jsx. CardGrid component has an array of gif ids

---
+++ src/components/CardGrid.jsx
@@ -0,0 +1,12 @@
+import {useState} from "react"
+import MemoryCard from "./MemoryCard.jsx"
+
+export default function CardGrid(){
+    const gifIds = [
+        "9hR8Dv4tYNYeWriXXC",
+        "GGojpBZaxHNkQTpsGO",
+        "l7emOdzh4LcUx133Pl",
+        "a5viI92PAF89q",
+        
+    ]
+}
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,14 +1,12 @@
-
-
 export default function MemoryCard({gifId}){
     const gif = fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=3jCZ9tx5mumrMUeoRZXCygZzChJIb82z`).then(
         res => res.json()
-    ).then(data => data.data.images.original.url)
+    ).then(data => { return {imgSrc: data.data.images.original.url, imgName: data.data.title}})
 
     return (
         <div className="card">
-            <img src={gif} alt=""></img>
-            <h2>{"Invincible"}</h2>
+            <img src={gif.imgSrc} alt=""></img>
+            <h2>{gif.imgName}</h2>
         </div>
     )
 }
