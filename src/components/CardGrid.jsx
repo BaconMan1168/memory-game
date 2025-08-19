6f1a434c9f364d93b557436cf96b0cb65834dcc3
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-08-19 19:56:40 +0800

    Each click now shuffles the memory cards

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -9,6 +9,17 @@ export default function CardGrid({clicked, onClick, currScore, onScoreChange}){
 
     ]
 
+    function createScrambledArr(){
+        const randomArr = [];
+        while (randomArr.length < 4){
+            let randomNum = Math.floor(Math.random() * 4);
+            if (!randomArr.includes(randomNum)){
+                randomArr.push(randomNum);
+            }
+        }
+        return randomArr;
+    }
+
     function handleClick(clickedId, alreadyClicked){
         if (clicked.length === 4){
             const empty = [];
@@ -25,22 +36,25 @@ export default function CardGrid({clicked, onClick, currScore, onScoreChange}){
             copy.push(clickedId);
             onClick(copy);
             onScoreChange(currScore + 1);
+        }    
+    }
+
+    const scrambledArr = createScrambledArr();
+    const cardArr = [];
+
+    while (scrambledArr.length > 0){
+        let index = scrambledArr.shift();
+        if (clicked.includes(gifIds[index])){
+            cardArr.push(<MemoryCard key={`id-${gifIds[index]}`} gifId={gifIds[index]} isClicked={true} onClick={handleClick} />) 
         }
-        
+        else{
+            cardArr.push(<MemoryCard key={`id-${gifIds[index]}`} gifId={gifIds[index]} isClicked={false} onClick={handleClick} />)
+        }
     }
 
     return (
         <div className="grid">
-            {gifIds.map(id => {
-
-            if (clicked.includes(id)){
-                return <MemoryCard key={`id-${id}`} gifId={id} isClicked={true} onClick={handleClick} />
-            }
-            else{
-                return <MemoryCard key={`id-${id}`} gifId={id} isClicked={false} onClick={handleClick} />
-            }
-
-            })}
+            {cardArr};
         </div>
         
             
